f103ef905d6e5c2e29d017922f31dcf84c2b3159
Brian Mai <0hrs3g@gmail.com>
2023-12-17 23:29:17 -0800

    cpp template

--- _main.cpp
+++ _main.cpp
@@ -28,6 +28,10 @@ void solve() {
 }
 
 int main() {
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
     ios_base::sync_with_stdio(0);
     cin.tie(0); cout.tie(0);
     int tc = 1;
