bdf2f13b0b10f88d423731f8bee870c00eccb9e7
Brian Mai <0hrs3g@gmail.com>
2023-12-21 11:37:02 -0800

    process input

---
+++ 800/282A.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    #ifndef ONLINE_JUDGE
+        freopen("../input.txt", "r", stdin);
+        freopen("../output.txt", "w", stdout);
+    #endif
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    int n;
+    cin >> n;
+    string s;
+    int res = 0;
+    for (int i = 0; i < n; ++i) {
+        cin >> s;
+        res += (s == "++X" || s == "X++");
+        if (s == "--X" || s == "X--") --res;
+    }
+    cout << res;
+}
