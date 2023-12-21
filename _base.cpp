8fd34b3a78e7fc583a8b514ec285ca081afad234
Brian Mai <0hrs3g@gmail.com>
2023-12-21 11:36:37 -0800

    fix filepath

--- _base.cpp
+++ _base.cpp
@@ -7,8 +7,8 @@ void solve() {
 
 int main() {
     #ifndef ONLINE_JUDGE
-        freopen("input.txt", "r", stdin);
-        freopen("output.txt", "w", stdout);
+        freopen("../input.txt", "r", stdin);
+        freopen("../output.txt", "w", stdout);
     #endif
     ios_base::sync_with_stdio(0);
     cin.tie(0); cout.tie(0);
