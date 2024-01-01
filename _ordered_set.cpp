7c340042d8ed86a654bd8c84dbed3e033242fd73
Brian Mai <0hrs3g@gmail.com>
2024-01-01 14:16:17 -0800

    ordered_set include

---
+++ _ordered_set.cpp
@@ -0,0 +1,7 @@
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+ 
+typedef __gnu_pbds::tree<int, __gnu_pbds::null_type, less<int>, __gnu_pbds::rb_tree_tag, __gnu_pbds::tree_order_statistics_node_update> ordered_set;
+
+// order_of_key (k) : Number of items strictly smaller than k 
+// find_by_order(k) : K-th element in a set (counting from zero).
